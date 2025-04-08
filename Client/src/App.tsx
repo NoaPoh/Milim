dbf796073b2b2ad7771cf0bfadfaba5a28c8c926
noasasson <noasasson18@gmail.com>
2025-04-08 20:35:02 +0300

    for now

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -5,20 +5,20 @@ import Register from './pages/Register/Register';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Camera from './pages/Camera/Camera';
+import { RoutesValues } from './consts/routes';
 
 const App = () => {
   const location = useLocation();
 
   return (
-    <div className="min-h-screen bg-[#FBF3DF] text-gray-900">
-      {location.pathname !== '/register' && location.pathname !== '/login' && (
-        <Navbar />
-      )}
+    <div>
+      {location.pathname !== RoutesValues.REGISTER &&
+        location.pathname !== RoutesValues.LOGIN && <Navbar />}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/camera" element={<Camera />} />
+        <Route path={RoutesValues.HOME} element={<Home />} />
+        <Route path={RoutesValues.REGISTER} element={<Register />} />
+        <Route path={RoutesValues.LOGIN} element={<Login />} />
+        <Route path={RoutesValues.CAMERA} element={<Camera />} />
       </Routes>
     </div>
   );
---
+++ Client/src/consts/routes.ts
@@ -0,0 +1,14 @@
+export enum RoutesValues {
+  HOME = '/',
+  REGISTER = '/register',
+  LOGIN = '/login',
+  CAMERA = '/camera',
+}
+
+export enum ObjectsCategories {
+  AIRPORT = '/categories/airport',
+  SCHOOL = '/categories/school',
+  FOOD = '/categories/food',
+  GARDEN = '/categories/garden',
+  BEDROOM = '/categories/bedroom',
+}
