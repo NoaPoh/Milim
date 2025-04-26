1be64af8271b05ad4bcd02c3a7126469539e4059
johnny <324825470@idf.il>
2025-04-26 17:42:47 +0300

    add migration scripts for Animal table with price column and initial data

---
+++ Server/src/core/prisma/migrations/add_animals/migration.sql
@@ -0,0 +1,14 @@
+INSERT INTO "Animal" ("id", "name", "price" , "imagePath" )
+VALUES (1, 'antilope', 100, 'antilope.png'),
+        (2, 'boar', 100, 'boar.png'),
+        (3, 'cheetah', 100, 'cheetah.png'),
+        (4, 'crocodile', 0, 'crocodile.png'),
+        (5, 'elephant', 100, 'elephant.png'),
+        (6, 'hippo', 0, 'hippo.png'),
+        (7, 'jiraffe', 0, 'jiraffe.png'),
+        (8, 'lion', 100, 'lion.png'),
+        (9, 'monkey', 0, 'monkey.png'),
+        (10, 'tiger', 100, 'tiger.png'),
+        (11, 'zebra', 100, 'zebra.png')
+        ;
+
