f5d7f344fcb290356a303781394b36e179e3e31f
נועה <noa@>
2025-04-26 19:22:58 +0300

    pull from main

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -10,16 +10,16 @@ const Navbar = () => {
   return (
     <nav className="fixed bottom-0 w-full flex justify-around p-3 border-t border-gray-300">
       <Link to="/games" className="text-gray-900">
-        <img src="/icons/gameboy-icon.svg" />
+        <SportsEsportsIcon />
       </Link>
       <Link to="/" className="text-gray-900">
-        <img src="/icons/home-icon.svg" />
+        <HomeIcon />
       </Link>
       <Link to="/camera" className="text-gray-900">
-        <img src="/icons/camera-icon.svg" />
+        <PhotoCameraIcon />
       </Link>
       <Link to="/profile" className="mx-2">
-        <img src="/icons/profile-icon.svg" />
+        <PersonIcon />
       </Link>
     </nav>
   );
