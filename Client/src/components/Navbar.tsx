ea80be7cf466b50fc25c8eef081a12516a280897
נועה <noa@>
2025-04-26 19:22:02 +0300

    pull from main

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -8,19 +8,18 @@ import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    //bottom navigation bar
-    <nav className="navbar">
-      <Link to="/" className="navbar__link">
-        <HomeIcon />
+    <nav className="fixed bottom-0 w-full flex justify-around p-3 border-t border-gray-300">
+      <Link to="/games" className="text-gray-900">
+        <img src="/icons/gameboy-icon.svg" />
       </Link>
-      <Link to="/games" className="navbar__link">
-        <SportsEsportsIcon />
+      <Link to="/" className="text-gray-900">
+        <img src="/icons/home-icon.svg" />
       </Link>
-      <Link to="/camera" className="navbar__link">
-        <PhotoCameraIcon />
+      <Link to="/camera" className="text-gray-900">
+        <img src="/icons/camera-icon.svg" />
       </Link>
-      <Link to="/profile" className="navbar__link navbar__link--spaced">
-        <PersonIcon />
+      <Link to="/profile" className="mx-2">
+        <img src="/icons/profile-icon.svg" />
       </Link>
     </nav>
   );
