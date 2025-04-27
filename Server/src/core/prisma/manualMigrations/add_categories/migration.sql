ce4e2fb8ea7ceb304782adc9f67b9cf96233d994
johnny <324825470@idf.il>
2025-04-27 16:12:04 +0300

    remove createdbysystem from category and rearrange migrations

---
+++ Server/src/core/prisma/manualMigrations/add_categories/migration.sql
@@ -0,0 +1,10 @@
+INSERT INTO "Category" ("id", "name", "createdById" )
+VALUES (1, 'Airport', NULL),
+       (2, 'School', NULL),
+        (3, 'Park', NULL),
+        (4, 'Kitchen', NULL),
+        (5, 'Bedroom', NULL),
+        (6, 'Supermarket', NULL),
+        (7, 'Living Room', NULL)
+        ;
+
