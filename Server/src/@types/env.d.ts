96bc6d54e2db2376661f5e162e941777b0fcfc49
johnny <324825470@idf.il>
2025-04-29 01:38:05 +0300

    refactor: update type definitions and restructure type exports

--- Server/src/@types/env.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: 'development' | 'production' | 'test';
-    DATABASE_URL: string;
-    JWT_ACCESS_SECRET: string;
-    JWT_ACCESS_EXPIRATION: string;
-    CLIENT_URL: string;
-  }
-}
