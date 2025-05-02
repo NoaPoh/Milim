c4a4af7b019c9684cb56a7efb050c31329df10ac
johnny <324825470@idf.il>
2025-05-03 00:43:13 +0300

    remove unused useState import for cleaner code

--- Client/src/pages/Camera/Camera.tsx
+++ Client/src/pages/Camera/Camera.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import './Camera.scss';
 import useCameraFeed from './hooks/useCameraFeed';
 import useObjectDetection from './hooks/useObjectDetection';
--- Client/src/pages/Camera/components/Feed/CameraFeed.tsx
+++ Client/src/pages/Camera/components/Feed/CameraFeed.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import { forwardRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCamera } from '@fortawesome/free-solid-svg-icons';
 
