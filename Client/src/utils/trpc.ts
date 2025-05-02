b21d1cb367f9e9c41191adef937fd74ecb8509ef
johnny <324825470@idf.il>
2025-05-02 20:08:09 +0300

    Add handleUnauthorizedLink to redirect unauthenticated users to login

--- Client/src/utils/trpc.ts
+++ Client/src/utils/trpc.ts
@@ -1,11 +1,38 @@
 import { createTRPCReact } from '@trpc/react-query';
 import type { AppRouter } from 'milim-server';
-import { httpBatchLink } from '@trpc/client';
+import { httpBatchLink, TRPCClientError, TRPCLink } from '@trpc/client';
+import { observable } from '@trpc/server/observable';
 
 export const trpc = createTRPCReact<AppRouter>();
 
+// like a middleware, but for the client
+// this will redirect the user to the login page if they are not authenticated
+export const handleUnauthorizedLink: TRPCLink<AppRouter> = () => {
+  return ({ next, op }) => {
+    return observable((observer) => {
+      const unsubscribe = next(op).subscribe({
+        next(value) {
+          observer.next(value);
+        },
+        error(err) {
+          if (err instanceof TRPCClientError && err.data?.httpStatus === 401) {
+            window.location.href = '/login';
+          }
+          observer.error(err);
+        },
+        complete() {
+          observer.complete();
+        },
+      });
+
+      return unsubscribe;
+    });
+  };
+};
+
 export const trpcClient = trpc.createClient({
   links: [
+    handleUnauthorizedLink,
     httpBatchLink({
       url: import.meta.env.VITE_API_URL,
       fetch(url, options) {
