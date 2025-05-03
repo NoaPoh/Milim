2125160014e2369769c9e2a764813be3bef52be5
נועה <noa@>
2025-05-03 23:25:32 +0300

    only activate user context if on pages of logged in user

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import { trpc, trpcClient } from './utils/trpc/trpc';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
@@ -9,17 +9,29 @@ import './index.css';
 import { UserProvider } from './context/UserContext';
 
 const queryClient = new QueryClient();
+const client = trpcClient(queryClient); // Initialize the trpc client with the query client
+
+// Wrap only if necessary (decide before rendering)
+const AppWithAuth = () => {
+  const location = useLocation();
+  const isAuthPage = /^\/(login|register)$/.test(location.pathname);
+
+  // Do not wrap the App component in UserProvider if we are on the login/register pages
+  return isAuthPage ? <App /> : (
+    <UserProvider>
+      <App />
+    </UserProvider>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <trpc.Provider client={client} queryClient={queryClient}>
         <QueryClientProvider client={queryClient}>
-          <UserProvider>
-            <App />
-          </UserProvider>
+          <AppWithAuth />
         </QueryClientProvider>
       </trpc.Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
