511644f30c81d2f30e366ff24681e1bc01b4740f
unknown <danielnissan2002@gmail.com>
2025-05-13 20:34:06 +0300

    create answers box

--- Client/src/pages/Games/FlashCards/FlashCards.tsx
+++ Client/src/pages/Games/FlashCards/FlashCards.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import './FlashCards.scss';
 
 const FlashCards = () => {
-  return <div>FlashCards</div>;
+  const answers = [
+    { id: 1, answer: 'Answer 1' },
+    { id: 2, answer: 'Answer 2' },
+    { id: 3, answer: 'Answer 3' },
+    { id: 4, answer: 'Answer 4' },
+  ];
+  return (
+    <>
+      <img src="rrtrtrtr" alt="chair" className="w-19 h-19 category-icon" />
+      <div className="flashcards-container">
+        {answers.map((item) => (
+          <div key={item.id} className="flashcards-container__card">
+            {item.answer}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default FlashCards;
