49b72219c2deda9b36063c0b43f2a44e9aaed0db
noasasson <noasasson18@gmail.com>
2025-05-14 19:41:13 +0300

    includes changes

--- Client/src/routes/router.tsx
+++ Client/src/routes/router.tsx
@@ -10,6 +10,8 @@ import GamesHome from '../pages/Games/GamesHome/GamesHome';
 import Crossword from '../pages/Games/Crossword/Crossword';
 import FlashCards from '../pages/Games/FlashCards/FlashCards';
 import Spelling from '../pages/Games/Spelling/Spelling';
+import WelcomeToGame from '../pages/Games/WelcomeScreen/WelcomeToGame';
+// import Profile from '../pages/Profile/Profile';
 
 const Page = ({ children }: { children: React.ReactNode }) => {
   return <div className="page-w-navbar">{children}</div>;
@@ -54,18 +56,28 @@ const routes: RouteProps[] = [
   {
     path: GamesRoutesValues.CROSSWORD,
     component: <Crossword />,
-    navbar: true,
+    navbar: false,
   },
   {
     path: GamesRoutesValues.FLASH_CARDS,
     component: <FlashCards />,
-    navbar: true,
+    navbar: false,
   },
   {
     path: GamesRoutesValues.SPELLING,
     component: <Spelling />,
-    navbar: true,
+    navbar: false,
   },
+  {
+    path: RoutesValues.GAME_WELCOME_SCREEN,
+    component: <WelcomeToGame />,
+    navbar: false,
+  },
+  // {
+  //   path: RoutesValues.PROFILE,
+  //   component: <Profile />,
+  //   navbar: true,
+  // },
 ];
 
 interface RouterProps {}
