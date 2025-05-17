d3f7751c4e87d6530fdba5c6ddefa450440b8f99
johnny <324825470@idf.il>
2025-05-17 22:35:02 +0300

    Update package.json files to streamline dependencies and improve workspace structure

--- Client/src/utils/trpcClient.ts
+++ Client/src/utils/trpcClient.ts
@@ -4,6 +4,7 @@ import { httpBatchLink } from '@trpc/client';
 import { resetUserCacheAfterChange } from './trpc/invalidationLink';
 import { QueryClient } from '@tanstack/react-query';
 import { handleUnauthorizedLink } from './trpc/handleUnauthorizedLink';
+import superjson from 'superjson';
 
 export const api = createTRPCReact<AppRouter>();
 
@@ -12,6 +13,7 @@ export const trpcClient = (queryClient: QueryClient) =>
     links: [
       handleUnauthorizedLink,
       httpBatchLink({
+        transformer: superjson,
         url: import.meta.env.VITE_API_URL,
         fetch(url, options) {
           return fetch(url, {
