3d1d82aa9d0da9ed1dfdc04de1142e2a839a2dce
johnny <324825470@idf.il>
2025-05-17 22:37:37 +0300

    Refactor Category component by removing unused TensorFlow import; update package.json to reorder dependencies

--- Client/src/pages/Category/Category.tsx
+++ Client/src/pages/Category/Category.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import '@tensorflow/tfjs';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import './Category.scss';
