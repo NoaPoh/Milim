8acf661af5c2b7fb78a335febea37e61b28f30fd
noasasson <noasasson18@gmail.com>
2025-05-26 13:36:17 +0300

    game hook

---
+++ Client/src/pages/Games/hooks/useGames.ts
@@ -0,0 +1,21 @@
+import { api } from '../../../utils/trpcClient';
+import { games } from '../../../constants/games';
+import { useEffect, useState } from 'react';
+
+interface props {
+  game: (typeof games)[number]['name'];
+}
+
+export const useGames = ({ game }: props) => {
+  const [amount, setAmount] = useState<number>(5);
+
+  useEffect(() => {
+    game === 'flashcards' && setAmount(20);
+  }, [game]);
+
+  const { data: words } = api.word.fetchRandomUserWords.useQuery({
+    amount,
+  });
+
+  return { words };
+};
