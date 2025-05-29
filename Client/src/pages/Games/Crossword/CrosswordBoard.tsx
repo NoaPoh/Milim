4b5ccd6cc3bb6b9cba42e64f911e0641d5805b32
johnny <324825470@idf.il>
2025-05-29 20:58:19 +0300

    Refactor import statements in CrosswordBoard component

--- Client/src/pages/Games/Crossword/CrosswordBoard.tsx
+++ Client/src/pages/Games/Crossword/CrosswordBoard.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useCrossword } from '../Hooks/useCrossword';
+import { useState } from 'react';
+import { useCrossword } from '../hooks/useCrossword';
 import './CrosswordBoard.scss';
 
 interface Props {
