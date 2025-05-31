283573c03438f2e44681ac80783d3d5b1a900b83
נועה <noa852002@gmail.com>
2025-05-31 13:16:48 +0300

    pass active user icon style purchases to component

--- Client/src/components/AnimalIcon/AnimalIcon.tsx
+++ Client/src/components/AnimalIcon/AnimalIcon.tsx
@@ -7,17 +7,24 @@ interface Props {
   iconWidth: number;
 }
 
+export const FrameToStyle = new Map<string, string>([
+  ['dotted frame', '1px black dotted'],
+  ['dashed frame', '2px red dashed'],
+  ['solid frame', '2px black solid'],
+]);
 interface Accessory {
   style: React.CSSProperties;
   name: string;
+  background: string;
+  frame: string;
 }
 
-const AnimalIcon = ({ path, iconWidth }: Props) => {
+const AnimalIcon = ({ path, iconWidth, background, frame }: Props) => {
   return (
     <img
       src={`src/assets/images/animals/${path}`}
       className={`animal rounded-full`}
-      style={{ width: `${iconWidth}px` }}
+      style={{ width: `${iconWidth}px`, backgroundColor: background, border: `${FrameToStyle.get(frame)}` }}
       alt="missing your photo!"
     />
   );
