24f8d37cf4c96db6f39a52bd95ede035d12ab8db
נועה <noa852002@gmail.com>
2025-05-31 13:09:51 +0300

    add query for activating award

--- Server/src/features/award/award.router.ts
+++ Server/src/features/award/award.router.ts
@@ -1,15 +1,15 @@
 import { z } from 'zod';
-import { publicProcedure } from '../../core/trpc/trpc';
+import { protectedProcedure, publicProcedure } from '../../core/trpc/trpc';
 import { awardService } from './award.service';
-import { Award, AwardType } from '@prisma/client';
 import { router } from '../../core/trpc/trpc';
 
 export const awardRouter = router({
   getAll: publicProcedure.query(async ({ ctx}) => {
     return awardService.getAllAwards(ctx.prisma);
   }),
-
-  getByType: publicProcedure.input(z.nativeEnum(AwardType)).query(async ({ ctx, input  }) => {
-    return awardService.getAwardsByType(ctx.prisma, input);
-  }),
+  useAward: protectedProcedure
+    .input(z.object({ awardId: z.number() }))
+    .mutation(async ({ input, ctx }) => {
+      return await awardService.useAward(ctx.prisma ,ctx.userId, input.awardId);
+    }),
 });
--- Server/src/features/award/award.service.ts
+++ Server/src/features/award/award.service.ts
@@ -5,9 +5,35 @@ export const awardService = {
     return prisma.award.findMany();
   },
 
-  getAwardsByType: async (prisma: PrismaClient, type: AwardType) => {
-    return prisma.award.findMany({
-      where: { category: type },
+  useAward: async (prisma: PrismaClient, userId: number, awardId: number) => {
+    const purchase = await prisma.purchase.findUnique({
+      where: {
+        userId_awardId: {
+          userId,
+          awardId,
+        },
+      },
     });
+
+    if (!purchase) {
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: 'You don’t own this award.',
+      });
+    }
+
+    await prisma.purchase.update({
+      where: {
+        userId_awardId: {
+          userId,
+          awardId,
+        },
+      },
+      data: {
+        createdAt: new Date(),
+      },
+    });
+
+    return { success: true };
   },
 };
