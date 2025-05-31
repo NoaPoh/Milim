cf777d92cb1885d9460af17d44d9ae7e940850b1
noasasson <noasasson18@gmail.com>
2025-05-31 19:21:54 +0300

    in client side

--- Client/src/pages/Games/hooks/useGames.ts
+++ Client/src/pages/Games/hooks/useGames.ts
@@ -1,16 +1,26 @@
 import { api } from '../../../utils/trpcClient';
 import { GameNames, games } from '../../../constants/games';
-import { useEffect } from 'react';
 
 interface props {
   game: (typeof games)[number]['name'];
 }
 
-export const useGames = (props: props) => {
-  const amount = props.game === GameNames.FLASH_CARDS ? 5 : 10;
+export const useGames = ({ game }: props) => {
+  const amount = game === GameNames.FLASH_CARDS ? 10 : 5;
+
   const { data: words = [], isLoading } =
     api.word.fetchRandomUserWords.useQuery({
-      amount,
+      amount: amount,
+      noSpaceLimitation:
+        game === GameNames.CROSSWORD || game === GameNames.SPELLING
+          ? true
+          : false,
+      charsLimitation:
+        game === GameNames.CROSSWORD
+          ? 6
+          : game === GameNames.SPELLING
+          ? 10
+          : undefined,
     });
 
   return { words, isLoading };
