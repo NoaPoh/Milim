ee6582aa36e0181f9bd8fe4d5c94d12ebc3bf95f
נועה <noa852002@gmail.com>
2025-06-10 23:46:08 +0300

    add speaker button and word translation to word modal

--- Client/src/pages/Category/components/WordModal/WordModal.tsx
+++ Client/src/pages/Category/components/WordModal/WordModal.tsx
@@ -1,4 +1,5 @@
 import Modal from '../../../../components/Modal/Modal';
+import SpeakerButton from '../../../../components/SpeakerButton.tsx';
 
 export interface ClickedWord {
   wordId: number;
@@ -18,14 +19,16 @@ export const WordModal = (props: WordModalProps) => {
       {props.wordId && (
         <Modal>
           <div
-            className="WordCard__container WordCard__modal"
-            onClick={() => props.closeModal()}
-          >
+            className="WordCard__container WordCard__modal">
             <img
               src={props.picture}
               alt={props.originalText}
               className="object-cover"
             />
+            <div className="hebrew-row">
+              <p>{props.translatedText}</p>
+              <SpeakerButton text={props.translatedText} language="he-IL" />
+            </div>
             <p>{props.originalText}</p>
             <button
               className="WordCard__finish-button"
